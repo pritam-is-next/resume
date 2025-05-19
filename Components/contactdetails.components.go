8dd72a7c743f7a114b3ecb25e31b0797a6f41435
Pritam Dutta <profitslate@gmail.com>
2025-05-19 18:48:26 +0000

    Refactor ContactDetails struct for improved readability and consistency

--- Components/contactdetails.components.go
+++ Components/contactdetails.components.go
@@ -1,20 +1,21 @@
 package components
 
 var ContactDetails = struct {
-	Email     string
-	Github    string
+	Email         string
+	Github        string
 	GithubVrianta string
-	LinkedIn  string
-	Facebook string
-	Twitter   string
-	Instagram string
-	Location  string
-	Phone     string
+	LinkedIn      string
+	Facebook      string
+	Twitter       string
+	Instagram     string
+	Location      string
+	Phone         string
 }{
-	Email:     "pritam.dutta@vrianta.in",
-	Github:    "",
-	LinkedIn:  "https://www.linkedin.com/in/--pritam-dutta/",
-	Instagram: "https://www.instagram.com/_mr.personal/",
-	Location:  "Kolkata, India",
-	Phone:     "+91 9163841954",
+	Email:         "pritam.dutta@vrianta.in",
+	Github:        "https://github.com/pritam-is-next?tab=repositories",
+	GithubVrianta: "https://github.com/vrianta?tab=repositories",
+	LinkedIn:      "https://www.linkedin.com/in/--pritam-dutta/",
+	Instagram:     "https://www.instagram.com/_mr.personal/",
+	Location:      "Kolkata, India",
+	Phone:         "+91 9163841954",
 }
