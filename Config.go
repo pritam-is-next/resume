c61a9a747c932afd5205516376525f9e2f9c585d
pritam-is-next <rohitdutta907@gmail.com>
2025-05-31 02:46:50 +0530

    create a seperetate file for configs

---
+++ Config.go
@@ -0,0 +1,16 @@
+package main
+
+import server "github.com/vrianta/Server"
+
+var Config = &server.Config{
+	JS_Folders: []string{
+		"Js",
+	},
+	CSS_Folders: []string{
+		"Css",
+	},
+	Static_folders: []string{
+		"Static",
+	},
+	Views_folder: "Views",
+}
--- main.go
+++ main.go
@@ -5,16 +5,5 @@ import (
 )
 
 func main() {
-	server.New("", "1080", Routes, &server.Config{
-		JS_Folders: []string{
-			"Js",
-		},
-		CSS_Folders: []string{
-			"Css",
-		},
-		Static_folders: []string{
-			"Static",
-		},
-		Views_folder: "Views",
-	}).Start()
+	server.New("", "1080", Routes, Config).Start()
 }
